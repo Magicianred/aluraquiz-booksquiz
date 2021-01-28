74b88a90cecdb9d3ebd99884edefbc1ee8e7fdc5
Izabella Loyse <izabellaloyse13@gmail.com>
2021-01-28 18:14:42 -0300

    fix(project): Teste de deploy

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import db from "../db.json";
 import  Widget from '../src/components/Widget';
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
-import GitHubCorner from '../src/components/GitHubCorner'
+import GitHubCorner from '../src/components/GitHubCorner';
 
 
 
