281966b9b9bef3278f5f067fb41582968150722e
Izabella Loyse <izabellaloyse13@gmail.com>
2021-01-28 17:57:37 -0300

    feat(project): Adicionado as pastas do projeto

---
+++ pages/index.js
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+import db from "../db.json";
+import  Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner'
+
+
+
+
+
+/* const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+  max-width: 100%;
+`; */
+
+
+export const QuizContainer = styled.section`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+
+
+
+
+
+
+export default function Home() {
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+      <QuizLogo />
+
+        <Widget>
+          <Widget.Header>
+            <h1>Books Quiz</h1>
+          </Widget.Header>
+
+          <Widget.Content>
+            <p>
+              Teste  os seus conhecimentos sobre os livros nacionais e
+              internacionais...
+            </p>
+          </Widget.Content>
+        </Widget>
+
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p> Quizes do pessoal da Imersão React da Alura !</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/IzabellaLoyse/aluraquiz-booksquiz" />
+    </QuizBackground>
+  );
+}
