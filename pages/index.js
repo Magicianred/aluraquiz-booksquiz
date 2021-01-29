bfa9d56d3a7dbfff763ae1e5161c87cade643c3c
Izabella Loyse <izabellaloyse13@gmail.com>
2021-01-29 13:40:43 -0300

    fix(project): Implementação de meta tags

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import  Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
-//import GitHubCorner from '../src/components/GitHubCorner';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 
 
@@ -37,12 +37,13 @@ export default function Home() {
     <QuizBackground backgroundImage={db.bg}>
        <Head>
       <title>Books Quiz</title>
+      <meta property="og:type" content="website" key="type"/>
       <meta property="og:title" content="Books Quiz" key="title" />
-      <meta property="og:image" content="https://images.unsplash.com/photo-1521920592574-49e0b121c964?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" key="title" />
+      <meta property="og:url" content="https://aluraquiz-booksquiz.izabella-loyse20.vercel.app/z" key="url" />
+      <meta property="og:description" content="Teste os seus conhecimentos sobre os livros  nacionais e internacionais neste Quiz interativo." key="description"/>
+      <meta property="og:image" content="https://images.unsplash.com/photo-1521920592574-49e0b121c964?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" key="image" />
+
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    
-
-
     </Head>
 
       <QuizContainer>
@@ -50,14 +51,11 @@ export default function Home() {
 
         <Widget>
           <Widget.Header>
-            <h1>Books Quiz</h1>
+            <h1>{db.title}</h1>
           </Widget.Header>
 
           <Widget.Content>
-            <p>
-              Teste  os seus conhecimentos sobre os livros nacionais e
-              internacionais...
-            </p>
+            <p>{db.description}</p>
           </Widget.Content>
         </Widget>
 
@@ -66,12 +64,12 @@ export default function Home() {
           <Widget.Content>
             <h1>Quizes da Galera</h1>
 
-            <p> Quizes do pessoal da Imersão React da Alura !</p>
+            <p> Quiz do pessoal da Imersão React da Alura !</p>
           </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
-      {/* /<GitHubCorner projectUrl="https://github.com/IzabellaLoyse/aluraquiz-booksquiz" /> */}
+       <GitHubCorner projectUrl="https://github.com/IzabellaLoyse/aluraquiz-booksquiz" /> 
     </QuizBackground>
     
   );
